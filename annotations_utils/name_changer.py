8bf1630fa5dff1c66c1cf1ec203613fdac0225a0
Ana Pinto <anapinto22012003@gmail.com>
2024-12-03 23:31:33 +0000

    some test_set

--- annotations_utils/name_changer.py
+++ annotations_utils/name_changer.py
@@ -1,20 +1,20 @@
 import os
 
 # Folder containing the images
-folder_path = r"C:\Users\anoca\Documents\GitHub\analogic-watch-detector\clock_second_hand\samples"
+folder_path = r"C:\Users\anoca\Documents\GitHub\analogic-watch-detector\test_set"
 
 # Starting number for the renaming
-start_number = 563
+start_number = 1
 
 # Get all .jpg files in the folder
-files = [f for f in os.listdir(folder_path) if f.lower().endswith('.png')]
+files = [f for f in os.listdir(folder_path) if f.lower().endswith('.jpg')]
 
 # Sort files to ensure consistent renaming
 files.sort()
 
 # Rename the files
 for i, file in enumerate(files):
-    new_name = f"watch{start_number + i}.jpg"
+    new_name = f"watch_test{start_number + i}.jpg"
     old_file = os.path.join(folder_path, file)
     new_file = os.path.join(folder_path, new_name)
     os.rename(old_file, new_file)
